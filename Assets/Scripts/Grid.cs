674296684ff466e6b9a5c191172cdb2ebb3476f8
wolfbrother9393 <wolfbrother9393@gmail.com>
2016-05-31 20:15:30 -0400

    add episode 05

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class Grid : MonoBehaviour {
 
-	public bool onlyDisplayPathGizmos;
+	public bool displayGridGizmos;
 	public LayerMask unwalkableMask;
 	public Vector2 gridWorldSize;
 	public float nodeRadius;
@@ -13,7 +13,7 @@ public class Grid : MonoBehaviour {
 	float nodeDiameter;
 	int gridSizeX, gridSizeY;
 
-	void Start() {
+	void Awake() {
 		nodeDiameter = nodeRadius*2;
 		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
 		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
@@ -69,29 +69,14 @@ public class Grid : MonoBehaviour {
 		int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
 		return grid[x,y];
 	}
-	
-	public List<Node> path;
+
 	void OnDrawGizmos() {
 		Gizmos.DrawWireCube (transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
-		if (onlyDisplayPathGizmos) {
-			if (path != null) {
-				foreach (Node n in path) {
-					Gizmos.color = Color.cyan;
-					Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-.1f));
-				}
-			}
-		}
-		else {
-
-			if (grid != null) {
-				foreach (Node n in grid) {
-					Gizmos.color = (n.walkable) ? Color.white : Color.red;
-					if (path != null)
-						if (path.Contains(n))
-							Gizmos.color = Color.cyan;
-					Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-.1f));
-				}
+		if (grid != null && displayGridGizmos) {
+			foreach (Node n in grid) {
+				Gizmos.color = (n.walkable) ? Color.white : Color.red;
+				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-.1f));
 			}
 		}
 	}
